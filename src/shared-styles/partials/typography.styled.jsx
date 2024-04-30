6eb3d3b083f03107d16d6d0c964500062083d9ee
Ewa Dworczak <edworczak@gmail.com>
2024-04-30 17:47:02 +0200

    Updated font sizes

--- src/shared-styles/partials/typography.styled.jsx
+++ src/shared-styles/partials/typography.styled.jsx
@@ -10,9 +10,9 @@ export const typography = {
 	fontFamilyCursive: "'Sacramento', cursive",
 	fontSize: {
 		default: "16px",
-		h1: "2.2em",
+		h1: "2.4em",
 		h2: "2em",
-		h3: "1.6em",
+		h3: "1.8em",
 	},
 	fontWeight: {
 		default: 300,
