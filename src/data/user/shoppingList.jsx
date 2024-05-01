5240a65b5411777e0e2443242a8daa65fa35507f
Ewa Dworczak <edworczak@gmail.com>
2024-05-01 22:37:56 +0200

    Created separate files for user data

---
+++ src/data/user/shoppingList.jsx
@@ -0,0 +1,24 @@
+export const shoppingList = {
+	threads: [
+		{
+			id: "dmc3799",
+		},
+		{
+			id: "dmc01",
+		},
+		{
+			id: "dmc644",
+		},
+	],
+	others: [
+		{
+			id: "item0001",
+			name: "nożyczki do haftu",
+		},
+		{
+			id: "item0002",
+			name: "bawełna surowa",
+			type: "fabrics",
+		},
+	],
+};
